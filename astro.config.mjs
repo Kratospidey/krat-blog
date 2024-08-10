6e8f9df131cbfdfa821871bf53924b707632728d
Kratospidey <devtools@kratospidey.com>
2024-08-11 00:56:32 +0530

    fix url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog-kratospidey.com",
+	site: "https://blog.kratospidey.com",
 	integrations: [
 		mdx(),
 		sitemap(),
